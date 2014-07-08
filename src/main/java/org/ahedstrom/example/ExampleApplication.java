012a1f06450e51d3cc858abcaba475751b6680d8
Henrik Stråth <henrik.strath@gmail.com>
2014-07-08 15:58:45 +0200

    Now uses DW 0.7.0 and Java 8

---
+++ src/main/java/org/ahedstrom/example/ExampleApplication.java
@@ -0,0 +1,23 @@
+package org.ahedstrom.example;
+
+import io.dropwizard.Application;
+import io.dropwizard.Configuration;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+public class ExampleApplication extends Application<Configuration> {
+
+    @Override
+    public void initialize(Bootstrap<Configuration> bootstrap) {
+    }
+
+    @Override
+    public void run(Configuration configuration, Environment environment) throws Exception {
+        environment.jersey().register(new Resource());
+        environment.getApplicationContext().addServlet(SseEventSourceServlet.class, "/sse");
+    }
+
+    public static void main(String[] args) throws Exception {
+        new ExampleApplication().run(args);
+    }
+}
